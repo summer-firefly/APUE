59187badf240feb31257feb8241b4bdd0755f01d
summer-firefly <summerisfirefly@gmail.com>
2024-11-08 10:35:53 +0800

    lseek usage

--- chapter03/src/lseek_use.c
+++ chapter03/src/lseek_use.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 void test01() {
-    //使用lseek获取当前文件偏移量
+    // 使用lseek获取当前文件偏移量
     int fd = open("./test.txt", O_RDWR | O_CREAT, 0666);
     if (fd < 0) {
         perror("open");
@@ -16,7 +17,32 @@ void test01() {
     printf("写入nice之后,文件的偏移量为%ld\n", off);
     close(fd);
 }
+void test02() {
+    // 使用lseek获取文件大小
+    int fd = open("./test.txt", O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return;
+    }
+    off_t off = lseek(fd, 0, SEEK_END);
+    printf("文件的大小为%ld\n", off);
+    close(fd);
+}
+void test03() {
+    // 测试lseek能否对标准输入设置偏移量
+    off_t off = lseek(STDIN_FILENO, 0, SEEK_CUR); // 尝试获取标准输入文件当前的偏移量
+    printf("off=%ld\n", off);
+    if (off == -1) {
+        perror("lseek");
+        printf("errno = %d\n", errno);
+        if (errno == ESPIPE) {
+            printf("errno被设置为ESPIPE\n");
+        }
+    }
+}
 int main() {
-    test01();
+    // test01();
+    // test02();
+    test03();
     return 0;
 }
